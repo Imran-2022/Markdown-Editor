ddc014de03be0a333ba484ae4181454c794d774d
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-04-08 12:39:20 +0600

    design app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Markdown from './pages/Markdown';
 function App() {
   return (
     <>
-    <p style={{textAlign: 'center',padding:"40px",background:"#140101c7",fontSize:"30px",color:"white" }}>REACT | MARKDOWN 😃</p>
-    <Markdown/>
+      <div style={{boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;' }}>
+        <p style={{ textAlign: 'center', padding: "40px", background: "#140101c7", fontSize: "30px", color: "white", }}>REACT | MARKDOWN 😃</p>
+      </div>
+      <Markdown />
+
     </>
   );
 }
