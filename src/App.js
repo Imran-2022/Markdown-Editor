f34e52e7e0b80bf1d392ed823ecd44b40846dc1d
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-04-08 15:10:22 +0600

    complete it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Markdown from './pages/Markdown';
 function App() {
   return (
     <>
-      <div style={{boxShadow: 'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;' }}>
+      <div>
         <p style={{ textAlign: 'center', padding: "40px", background: "#140101c7", fontSize: "30px", color: "white", }}>REACT | MARKDOWN 😃</p>
       </div>
       <Markdown />
--- src/pages/Markdown.js
+++ src/pages/Markdown.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Markdown.css"
+import ReactMarkdown from 'react-markdown'
 const Markdown = () => {
+    const [markdown, setMarkdown] = useState("")
+
     return (
         <div className="markdown">
-            <textarea name="" id="" cols="30" rows="10" autoFocus></textarea>
-            <div>lorem4</div>
+            <textarea name="" id="" cols="30" rows="10" value={markdown} onChange={(e) => setMarkdown(e.target.value)} autoFocus></textarea>
+            <div style={{ maxWidth: "500px", overflowWrap: 'break-word' }}>
+                <ReactMarkdown>{markdown}</ReactMarkdown>
+            </div>
         </div>
     );
 };
