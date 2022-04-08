cc69cd6a10dddd726d47d50ef358e18ff6f101cb
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-04-08 12:35:25 +0600

    design app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Markdown from './pages/Markdown';
+
 function App() {
   return (
     <>
-    <p>react markdown</p>
+    <p style={{textAlign: 'center',padding:"40px",background:"#140101c7",fontSize:"30px",color:"white" }}>REACT | MARKDOWN 😃</p>
+    <Markdown/>
     </>
   );
 }
---
+++ src/pages/Markdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Markdown.css"
+const Markdown = () => {
+    return (
+        <div className="markdown">
+            <div>lorem4</div>
+            <div>lorem4</div>
+        </div>
+    );
+};
+
+export default Markdown;
